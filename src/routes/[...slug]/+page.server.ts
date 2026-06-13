744d3eff19a3bd79f006c05edc5d4a59796f808d
Christian Wygoda <christian.wygoda@wygoda.net>
2026-06-13 20:53:16 +0200

    refactor: consolidate f8 page routes

--- src/routes/[...slug]/+page.server.ts
+++ src/routes/[...slug]/+page.server.ts
@@ -4,10 +4,13 @@ import { getF8PageEntries, loadF8Page } from '$lib/sveltekit/index.js';
 
 import type { EntryGenerator, PageServerLoad } from './$types.js';
 
-export const entries: EntryGenerator = () => getF8PageEntries();
+export const entries: EntryGenerator = () => [
+  { slug: '' },
+  ...getF8PageEntries()
+];
 
 export const load: PageServerLoad = ({ params, url }) => {
-  const page = loadF8Page({ slug: params.slug, origin: url.origin });
+  const page = loadF8Page({ slug: params.slug ?? '', origin: url.origin });
 
   if (page === undefined) {
     error(404, 'Page not found');
